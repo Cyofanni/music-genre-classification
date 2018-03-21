3772c27d22d5ea980d4e3e2deafbef9b8a97b6b9
Alex Beccaro <alex.becks@hotmail.it>
2018-03-21 18:06:03 +0100

    Added ROC graphs

--- src/log_reg.py
+++ src/log_reg.py
@@ -1,21 +1,44 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import normalize
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 
 from music_genre_mfcc import read_ceps
-from music_genre_fft import plot_confusion_matrix
+from plots import plot_confusion_matrix
+from plots import plot_roc_curves
+
+import numpy as np
 
 
 genre_list = ["classical", "jazz", "country", "pop", "rock", "metal"]
 
 
 if __name__ == "__main__":
+	# reading inputs
 	x, y = read_ceps(genre_list, "genres")
 
+	# logistic regression train and prediction
 	lr = LogisticRegression()
 	lr.fit(x, y)
 	y_pred = lr.predict(x)
 
+	# confusion matrix
 	cm = normalize(confusion_matrix(y, y_pred), axis=1, norm='l1')
-	print(cm)
 	plot_confusion_matrix(cm, genre_list, "Name", "Confusion Matrix")
+
+	# ROC curve
+	fprs = []
+	tprs = []
+	AUCs = []
+
+	for label in range(len(genre_list)):
+		y_label = np.asarray(y==label, dtype=int)
+		proba = lr.predict_proba(x)
+		proba_label = proba[:,label]
+		fpr, tpr, roc_thresholds = roc_curve(y_label, proba_label)
+		fprs.append(fpr)
+		tprs.append(tpr)
+		AUCs.append(roc_auc_score(y_label, proba_label))
+
+	plot_roc_curves(fprs, tprs, AUCs, genre_list)
---
+++ src/plots.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+
+
+def plot_confusion_matrix(cm, genre_list, name, title):
+	plt.clf()
+	plt.matshow(cm, fignum=False, cmap='Blues', vmin=0, vmax=1.0)
+	ax = plt.axes()
+	ax.set_xticks(range(len(genre_list)))
+	ax.set_xticklabels(genre_list)
+	ax.xaxis.set_ticks_position("bottom")
+	ax.set_yticks(range(len(genre_list)))
+	ax.set_yticklabels(genre_list)
+	plt.title(title)
+	plt.colorbar()
+	plt.grid(False)
+	plt.xlabel('Predicted class')
+	plt.ylabel('True class')
+	plt.grid(False)
+	plt.show()
+
+def plot_roc_curves(fpr, tpr, AUC, label):
+	f, axarr = plt.subplots(3, 2, figsize=(10, 10))
+	plt.xlabel('False Positive Rate')
+	plt.ylabel('True Positive Rate')
+
+	for r in range(3):
+		for c in range(2):
+			i = 2 * r + c
+
+			axarr[r][c].plot(fpr[i], tpr[i], 'lightblue')
+			axarr[r][c].fill_between(fpr[i], 0, tpr[i], facecolor='lightblue', alpha=0.5)
+			axarr[r][c].plot([0, 1], [0, 1], 'b--')
+			axarr[r][c].set_xlim([0.0, 1.0])
+			axarr[r][c].set_ylim([0.0, 1.0])
+			axarr[r][c].set_title('ROC curve for ' + label[i] + ' - AUC = %0.2f' % AUC[i])
+			# plt.savefig('Log_ROC')
+
+	plt.show()
