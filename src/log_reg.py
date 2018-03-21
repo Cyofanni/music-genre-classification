81133f22588bba48524019c14a5af1560eba6648
Alex Beccaro <alex.becks@hotmail.it>
2018-03-21 16:40:32 +0100

    Added confusion matrix

--- src/log_reg.py
+++ src/log_reg.py
@@ -1,6 +1,9 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import normalize
 
 from music_genre_mfcc import read_ceps
+from music_genre_fft import plot_confusion_matrix
 
 
 genre_list = ["classical", "jazz", "country", "pop", "rock", "metal"]
@@ -13,4 +16,6 @@ if __name__ == "__main__":
 	lr.fit(x, y)
 	y_pred = lr.predict(x)
 
-	print(y_pred)
+	cm = normalize(confusion_matrix(y, y_pred), axis=1, norm='l1')
+	print(cm)
+	plot_confusion_matrix(cm, genre_list, "Name", "Confusion Matrix")
